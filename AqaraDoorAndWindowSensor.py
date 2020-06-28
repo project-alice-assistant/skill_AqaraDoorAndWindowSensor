dd76bd9bacd05f20df383126a0973285e20ace4d
Psycho <laurentchervet@bluewin.ch>
2020-06-28 06:52:54 +0200

    Step one

--- AqaraDoorAndWindowSensor.py
+++ AqaraDoorAndWindowSensor.py
@@ -1,6 +1,10 @@
+from typing import Dict, Union
 from core.base.model.AliceSkill import AliceSkill
-from core.dialog.model.DialogSession import DialogSession
-from core.util.Decorators import IntentHandler
+
+try:
+	from skills.Zigbee2Mqtt.model.ZigbeeDeviceHandler import ZigbeeDeviceHandler
+except:
+	pass
 
 
 class AqaraDoorAndWindowSensor(AliceSkill):
@@ -9,6 +13,41 @@ class AqaraDoorAndWindowSensor(AliceSkill):
 	Description: Connect your aqara door and window sensors to alice. requires zigbee2mqtt
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+	def __init__(self):
+		super().__init__()
+		self._handler = None
+		self._state = None
+		self._battery = None
+		self._linkQuality = None
+
+
+	def onStart(self):
+		super().onStart()
+		self._handler = ZigbeeDeviceHandler(skillInstance=self.onDeviceMessage, modelId='lumi.sensor_magneet.aq2')
+
+
+	def onBooted(self) -> bool:
+		return self._handler.onBooted()
+
+
+	def onDeviceMessage(self, message: Union[str, Dict]):
+		if 'contact' in message:
+			self._state = message['contact']
+
+		print(f'my state is {self._state}')
+
+		if 'battery' in message:
+			self._battery = message['battery']
+
+		if 'linkquality' in message:
+			self._linkQuality = message['linkquality']
+
+
+	@property
+	def state(self) -> bool:
+		return self._state
+
+
+	@property
+	def battery(self) -> int:
+		return self._battery
